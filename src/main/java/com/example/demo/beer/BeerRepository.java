c5a2f8428a7e6ac5a94082e93021eb4dd6805026
gxedwards <gxedwards@gmail.com>
2018-12-29 10:40:40 -0500

    Added extra CORS settings

--- src/main/java/com/example/demo/beer/BeerRepository.java
+++ src/main/java/com/example/demo/beer/BeerRepository.java
@@ -2,11 +2,13 @@ package com.example.demo.beer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 /**
  * Leverages Spring Data to do CRUD on the Beer Entity
  * 
  * @RepositoryRestResource creates a RestEndpoint
  */
+@CrossOrigin(origins = {"http://localhost:4200","http://localhost:8100","http://localhost:8080"})
 @RepositoryRestResource
 interface BeerRepository extends JpaRepository<Beer, Long> {
 }
